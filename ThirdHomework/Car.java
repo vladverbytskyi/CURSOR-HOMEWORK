f3b1ab6a93bd23ce1ad232f82de7cbafa49eff6e
Vlad Verbytskyi <vlad.verbytskyi2019@gmail.com>
2019-12-05 19:53:20 +0200

    I add my ThirdHomework

---
+++ ThirdHomework/Car.java
@@ -0,0 +1,44 @@
+package ThirdHomework;
+
+public class Car {
+    boolean start_engine;
+    private int workEngine;
+    private int speed;
+    private int distance;
+
+    public void startEngine(boolean start_engine) {
+        if (start_engine = true) {
+            System.out.println("Good job, your engine get started");
+            while (workEngine < 10) {
+                System.out.println("Engine work " + workEngine + " second");
+                workEngine++;
+                if (workEngine == 10) {
+                    getStart();
+                }
+            }
+        } else {
+            System.out.println("Don`t worry, next time you can ");
+        }
+    }
+
+    public void getStart() {
+        System.out.println("Okay, lets go");
+        while (distance < 20) {
+            distance++;
+            speed = 100;
+            System.out.println("We drove by " + distance + " at speed " + speed + " km/h");
+
+            if (distance == 20) {
+                stop();
+
+
+            }
+        }
+    }
+
+    public void stop() {
+        speed = 0;
+        start_engine = false;
+        System.out.println("Engine off");
+    }
+}
---
+++ ThirdHomework/Run.java
@@ -0,0 +1,11 @@
+package ThirdHomework;
+
+public class Run {
+    public static void main(String[] args) {
+        new CountObject().countObject();
+        new Circle(1).circleArea();
+        new Car().startEngine(true);
+
+
+    }
+}
