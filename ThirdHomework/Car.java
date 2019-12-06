4b2a1fc069ab346d9c189a89a7062aca0c15ee6e
Vlad Verbytskyi <vlad.verbytskyi2019@gmail.com>
2019-12-07 01:54:29 +0200

    Refactor code

--- ThirdHomework/Car.java
+++ ThirdHomework/Car.java
@@ -1,41 +1,38 @@
 package ThirdHomework;
 
 public class Car {
-    boolean start_engine;
+    private boolean engineWork;
     private int workEngine;
     private int speed;
     private int distance;
 
-    public void startEngine(boolean start_engine) {
-        if (start_engine = true) {
+    public void startEngine() {
+        engineWork = true;
+        if (engineWork) {
             System.out.println("Good job, your engine get started");
             while (workEngine < 10) {
                 System.out.println("Engine work " + workEngine + " second");
                 workEngine++;
-                if (workEngine == 10) {
-                    getStart();
-                }
             }
         } else {
             System.out.println("Don`t worry, next time you can ");
         }
     }
-
-    public void getStart() {
+    public void startCar() {
         System.out.println("Okay, lets go");
         while (distance < 20) {
             distance++;
             speed = 100;
             System.out.println("We drove by " + distance + " at speed " + speed + " km/h");
-
-            if (distance == 20) {
-                stop();
-            }
         }
     }
-    public void stop() {
+    public void stopCar() {
         speed = 0;
-        start_engine = false;
         System.out.println("Engine off");
+    }
+    public void stopEngine() {
+        this.engineWork = false;
+        System.out.println("fdgjfbgjfm");
+
     }
 }
--- ThirdHomework/Run.java
+++
@@ -1,9 +0,0 @@
-package ThirdHomework;
-
-public class Run {
-    public static void main(String[] args) {
-        new CountObject().countObject();
-        new Circle(1).circleArea();
-        new Car().startEngine(true);
-    }
-}
