b32aef512668abe8272bfc4aa773a2206d7783a3
Vlad Verbytskyi <vlad.verbytskyi2019@gmail.com>
2019-12-07 12:38:38 +0200

    Delete //

--- ThirdHomework/CarAndCircleApplication.java
+++ ThirdHomework/CarAndCircleApplication.java
@@ -2,13 +2,13 @@ package ThirdHomework;
 
 public class CarAndCircleApplication {
     public static void main(String[] args) {
-       // new CountObject().countObject();
-//        new Circle(1).circleArea();
-//        new Car().startEngine();
+       new CountObject().countObject();
+       new Circle(1).circleArea();
+       
         Car car = new Car();
         car.startEngine();
         car.startCar();
-//        car.stopEngine();
+        car.stopEngine();
 
 
     }
