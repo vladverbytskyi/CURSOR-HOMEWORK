6cab05cdb8ef449de3e885f50738a593e14e4569
Vlad Verbytskyi <vlad.verbytskyi2019@gmail.com>
2019-12-15 22:00:08 +0200

    I push my hw6

---
+++ hw6/src/Run.java
@@ -0,0 +1,8 @@
+class Run {
+    static void runApp() {
+        new DivisibleBy3().divisibleBy3();
+        new Fruits().replaceFruits();
+        new CheckElement().checkSimilarElement();
+        new TreeSet().sortElement();
+    }
+}
---
+++ hw6/src/TreeSet.java
@@ -0,0 +1,17 @@
+import java.util.Comparator;
+import java.util.SortedSet;
+
+class TreeSet {
+    void sortElement() {
+        SortedSet<Integer> set = new java.util.TreeSet<>(Comparator.reverseOrder());
+        set.add(1);
+        set.add(22);
+        set.add(83);
+        set.add(44);
+        set.add(51);
+        set.add(15);
+
+
+        System.out.println("Sorted element " + set);
+    }
+}
