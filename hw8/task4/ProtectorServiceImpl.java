74faab82432916ada527f4b902101faac14d7adc
Vlad Verbytskyi <vlad.verbytskyi2019@gmail.com>
2019-12-22 22:48:25 +0200

    Added homework 8

---
+++ hw8/task4/ProtectorServiceImpl.java
@@ -0,0 +1,16 @@
+package task4;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ProtectorServiceImpl implements ProtectorService {
+    @Override
+    public List<Protector> protectThings(List<Box> boxes) {
+        return boxes.stream()
+                .flatMap(box -> box.getThings()
+                        .stream())
+                .filter(Thing::isFragile)
+                .map(Protector::new)
+                .collect(Collectors.toList());
+    }
+}
